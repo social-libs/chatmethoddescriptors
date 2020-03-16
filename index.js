3b3e1cef7d0aaae7f5f6bc75050adf16a2fb7bf2
Andrija Petrovic <andrija.hers@gmail.com>
2020-03-16 16:53:08 +0000

    Init

---
+++ index.js
@@ -0,0 +1,73 @@
+function createLib (execlib) {
+  return {
+    getNotifications: true,
+    processNewMessage: [{
+      name: 'From',
+      type: 'string'
+    },{
+      name: 'To Group',
+      type: 'String'
+    },{
+      name: 'To Peer',
+      type: 'String'
+    },{
+      name: 'Message',
+      type: 'String'
+    }],
+    createNewGroupWithMembers: [{
+      name: 'Username',
+      type: 'string'
+    },{
+      name: 'Group name',
+      type: 'string'
+    },{
+      name: 'Members',
+      type: ['array', 'null']
+    }],
+    getAllConversations: [{
+      name: 'Username',
+      type: 'string'
+    }],
+    initiateChatConversationsWithUsers: [{
+      name: 'Username',
+      type: 'string'
+    },{
+      name: 'Usernames',
+      type: ['array', 'null']
+    }],
+    getMessages: [{
+      name: 'User ID',
+      type: 'string'
+    },{
+      name: 'Conversation ID',
+      type: 'string'
+    },{
+      name: 'Oldest Message ID',
+      type: ['number', 'null']
+    },{
+      name: 'How Many',
+      type: ['number', 'null']
+    }],
+    markMessageRcvd: [{
+      name: 'User ID',
+      type: 'string'
+    },{
+      name: 'Conversation ID',
+      type: 'string'
+    },{
+      name: 'Message ID',
+      type: 'number'
+    }],
+    markMessageSeen: [{
+      name: 'User ID',
+      type: 'string'
+    },{
+      name: 'Conversation ID',
+      type: 'string'
+    },{
+      name: 'Message ID',
+      type: 'number'
+    }]
+  };
+}
+module.exports = createLib;
